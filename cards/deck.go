e5ac0723d8c314003bd9f8c0b7da5b1b1383309f
Rhian Castro <rhi.castro@gmail.com>
2020-11-25 00:05:18 -0300

    added functions: toString, saveToFile, newDeckFromFile, suffle.

---
+++ cards/deck.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"io/ioutil"
+	"fmt"
+	"strings"
+	"os"
+	"math/rand"
+)
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (cards deck) print() {
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(cards deck, handSize int) (deck, deck) {
+	return cards[:handSize], cards[handSize:]
+}
+
+func (cards deck) toString() string {
+	return strings.Join([]string(cards), ",")
+}
+
+func (cards deck) saveTofile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(cards.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	cardsString := strings.Split(string(bs), ",")
+	return deck(cardsString)
+}
+
+func (cards deck) shuffle() {
+	for i := range cards {
+		newPosition := rand.Intn(len(cards) - 1)
+		cards[i], cards[newPosition] = cards[newPosition], cards[i]
+	}
+}
